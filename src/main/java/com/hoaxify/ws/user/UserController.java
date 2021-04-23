f81471a540094a124accc4cd382b3508af45e142
Kaan Kara <kaan.kara@ozu.edu.tr>
2021-04-23 21:16:51 +0300

    validation is added

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,13 +1,19 @@
 package com.hoaxify.ws.user;
 
+import com.hoaxify.ws.user.error.ApiError;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -17,9 +23,30 @@ public class UserController {
     private UserService service;
 
     @PostMapping("/api/1.0/users")
-    public GenericResponse createUser(@RequestBody User user) {
-        service.save(user);
-        return new GenericResponse("User created!");
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        String username = user.getUsername();
+        String displayName = user.getDisplayName();
+        String password = user.getPassword();
+        Map<String,String> validationErrors = new HashMap<>();
+        ApiError err = new ApiError(400,"Validation Error","/api/1.0/users");
+        if(!StringUtils.isEmpty(username)&&!StringUtils.isEmpty(displayName)&&!StringUtils.isEmpty(password)){
+            service.save(user);
+            return ResponseEntity.ok("User created!");
+        }
+        if(StringUtils.isEmpty(username)) {
+            validationErrors.put("username","Username cannot be null.");
+
+        }
+        if(StringUtils.isEmpty(displayName)) {
+            validationErrors.put("displayName","Display name cannot be null.");
+
+        }
+        if(StringUtils.isEmpty(password)) {
+            validationErrors.put("password","Password cannot be null.");
+        }
+        err.setValidationErrors(validationErrors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+
 
     }
 
--- src/main/java/com/hoaxify/ws/user/UserService.java
+++ src/main/java/com/hoaxify/ws/user/UserService.java
@@ -1,18 +1,23 @@
 package com.hoaxify.ws.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository repository;
-
+    PasswordEncoder passwordEncoder;
 
     public UserService(UserRepository repository) {
         this.repository = repository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public void save(User user) {
+        String encryptedPassword = this.passwordEncoder.encode(user.getPassword());
+        user.setPassword(encryptedPassword);
         repository.save(user);
 
     }
