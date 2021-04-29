eab8b148f1ae19c03125163b87c6d5dc19f88fe4
Kaan Kara <kaan.kara@ozu.edu.tr>
2021-04-29 23:36:48 +0300

    Validations are added.

--- src/main/java/com/hoaxify/ws/user/User.java
+++ src/main/java/com/hoaxify/ws/user/User.java
@@ -4,8 +4,10 @@ import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Data
 @Entity
@@ -13,8 +15,14 @@ public class User {
     @Id
     @GeneratedValue
     private long id;
+    @NotNull(message = "Username cannot be null")
+    @Size(min = 4, max = 255, message = "Username must be between 4-255 characters.")
     private String username;
+    @NotNull(message = "Display name cannot be null")
+    @Size(min = 4, max = 255, message = "Display name must be between 4-255 characters.")
     private String displayName;
+    @NotNull(message = "Password cannot be null")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,16}$", message = "Password must be between 8-16 characters with at least one digit,one lower case letter and one upper case letter.")
     private String password;
 
 
--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -5,13 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,30 +21,24 @@ public class UserController {
     private UserService service;
 
     @PostMapping("/api/1.0/users")
-    public ResponseEntity<?> createUser(@RequestBody User user) {
-        String username = user.getUsername();
-        String displayName = user.getDisplayName();
-        String password = user.getPassword();
-        Map<String,String> validationErrors = new HashMap<>();
-        ApiError err = new ApiError(400,"Validation Error","/api/1.0/users");
-        if(!StringUtils.isEmpty(username)&&!StringUtils.isEmpty(displayName)&&!StringUtils.isEmpty(password)){
-            service.save(user);
-            return ResponseEntity.ok("User created!");
-        }
-        if(StringUtils.isEmpty(username)) {
-            validationErrors.put("username","Username cannot be null.");
+    public GenericResponse createUser(@Valid @RequestBody User user) {
+        service.save(user);
+        return new GenericResponse("User is created.");
 
-        }
-        if(StringUtils.isEmpty(displayName)) {
-            validationErrors.put("displayName","Display name cannot be null.");
 
-        }
-        if(StringUtils.isEmpty(password)) {
-            validationErrors.put("password","Password cannot be null.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> validationErrors = new HashMap<>();
+        ApiError err = new ApiError(400, "Validation Error", "/api/1.0/users");
+        for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
+            validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+
         }
         err.setValidationErrors(validationErrors);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
-
+        return err;
 
     }
 
